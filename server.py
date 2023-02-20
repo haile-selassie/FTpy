45df89a4c234b64f6de02fe95b9b44aaf7cc8e09
unknown <jsledgeph276@gmail.com>
2023-02-20 12:07:59 -0600

    made class connection

--- server.py
+++ server.py
@@ -4,24 +4,16 @@ import os
 LISTENING_HOST = ''
 LISTENING_PORT = 9001
 
-with socket.socket() as sock:
-    sock.setblocking(True)
-    sock.bind((LISTENING_HOST,LISTENING_PORT))
-
-    sock.listen()
-    print("FTpy server has been started.")
-
-    while True:
-        conn,addr = sock.accept()
-        client_ip = addr[0]
-        client_port = addr[1]
-        print(f"{client_ip} connected on port {client_port}")
-        
-        filelist = ""
-        for file in os.scandir("files"):
-            filelist += f"{file.name}\n"
-        conn.sendall(filelist.encode())
-
+class Connection:
+    def __init__(self,sock,addr):
+        self.sock = sock
+        self.ip = addr[0]
+        self.port = addr[1]
+        self.pwd = "files"
+        sock.setblocking(True)
+        print(f"{self.ip} connected on port {self.port}")
+    def download(self):
+        fn_len = conn.recv()
         filename = conn.recv(1024)
         filename = filename.decode()
 
@@ -32,5 +24,24 @@ with socket.socket() as sock:
                     break
                 conn.send(line)
 
-        print(f"{client_ip} has downloaded {filename}")        
+        print(f"{self.ip} has downloaded {filename}")
+    def filelist(self):
+        filelist = ""
+        for file in os.scandir(self.pwd):
+            filelist += f"{file.name}\n"
+        conn.sendall(filelist.encode())
+
+
+with socket.socket() as lsock:
+    lsock.setblocking(True)
+    lsock.bind((LISTENING_HOST,LISTENING_PORT))
+
+    lsock.listen()
+    print("FTpy server has been started.")
+
+    while True:
+        conn,addr = lsock.accept()
+        sock = Connection(conn,addr)
+        sock.filelist()
+        
         conn.close()
