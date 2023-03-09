dff9638078342c74e4b15f9430eae12c3d60e125
unknown <jsledgeph276@gmail.com>
2023-03-09 06:37:01 -0600

    added some gpl stuff

--- ftpy/client.py
+++ ftpy/client.py
@@ -1,20 +1,42 @@
+'''
+	Copyright (C) 2023 https://github.com/haile-selassie
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 import socket
 import os
 from utils import *
-import re
  
 PORT = int(input("Enter the port number.\n> "))
 HOST = input("Enter IP address of the server.\n> ")
 
 def main():
+	print("FTpy Copyright (C) 2023 https://github.com/haile-selassie")
+	print("This program comes with ABSOLUTELY NO WARRANTY; for details type `warranty'.")
+	print("This is free software, and you are welcome to redistribute it")
+	print("under certain conditions; see the GNU General Public License for more details.")
 	with socket.socket() as sock:
 			sock.setblocking(True)
 			sock.connect((HOST,PORT))
 			print(f"[FTpy] Connected to {HOST}:{PORT}.")
-
 			while True:
 				choice = input("FTpy> ").split()
+				if len(choice) == 0:
+					continue
 				command = choice[0]
+				if len(command) == 0:
+					continue
 				if command == "rls":
 					header_sendall(sock,LS_MESSAGE.encode(FORMAT))
 					file_list = header_recv(sock).decode().strip()
@@ -61,12 +83,21 @@ def main():
 								left-=BUFFER_SIZE
 				elif command == "exit":
 					break
+				elif command == "warranty":
+					print("THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.")
+					print("EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES")
+					print("PROVIDE THE PROGRAM “AS IS” WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR")
+					print("IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND")
+					print("FITNESS FOR A PARTICULAR PURPOSE. THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE")
+					print("OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE")
+					print("COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.")
 				elif command == "help":
 					print("help: show help")
 					print("ls: list local pwd")
 					print("rls: list remote pwd")
 					print("download FILENAME: download a file by name from server")
 					print("upload FILENAME: upload a file to server by name")
+					print("warranty: display warranty details")
 					print("exit: disconnect from server")
 				else:
 					print("Invalid command.")
