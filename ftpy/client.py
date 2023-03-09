78e55894239236fefa9ff48dd5e85b410737e372
unknown <jsledgeph276@gmail.com>
2023-03-09 06:05:19 -0600

    added more commands

--- ftpy/client.py
+++ ftpy/client.py
@@ -1,11 +1,10 @@
 import socket
 import os
 from utils import *
+import re
  
 PORT = int(input("Enter the port number.\n> "))
 HOST = input("Enter IP address of the server.\n> ")
-
-command = "pull"
 
 def main():
 
@@ -14,17 +13,59 @@ def main():
 			sock.connect((HOST,PORT))
 			print(f"[FTpy] Connected to {HOST}:{PORT}.")
 
-			if command == "rls":
-				header_sendall(sock,LS_MESSAGE.encode(FORMAT))
-				file_list = header_recv(sock).decode().strip()
-				print(file_list)
-			elif command == "ls":
-				pass
-			elif command == "pull":
-				header_sendall(sock,DOWNLOAD_MESSAGE.encode(FORMAT))
-				header_sendall(sock,"hello.txt".encode(FORMAT))
-				file_size = header_recv(sock).decode(FORMAT).strip()
-				file_size = int(file_size)
+			while True:
+				choice = input("FTpy> ").split()
+				command = choice[0]
+				if command == "rls":
+					header_sendall(sock,LS_MESSAGE.encode(FORMAT))
+					file_list = header_recv(sock).decode().strip()
+					print(file_list)
+				elif command == "ls":
+					file_list = "\n".join([file for file in os.listdir()])
+					print(file_list)
+				elif command == "download":
+					filename = choice[1]
+					header_sendall(sock,DOWNLOAD_MESSAGE.encode(FORMAT))
+					header_sendall(sock,filename.encode(FORMAT))
+					file_size = header_recv(sock).decode(FORMAT).strip()
+					file_size = int(file_size)
+					if file_size == 0:
+						print(f"[FTpy] ERROR: {filename} does not exist.")
+						continue
+					with open(filename,"wb") as newfile:
+						left = file_size
+						while left > 0:
+							if left < BUFFER_SIZE:
+								newfile.write(sock.recv(left))
+								left = 0
+							else:
+								newfile.write(sock.recv(BUFFER_SIZE))
+								left-=BUFFER_SIZE
+					print(f"[FTpy] Downloaded {filename} from {HOST}")
+				elif command == "upload":
+					filename = choice[1]
+					filepath = os.getcwd()+"/"+filename
+					if not os.path.exists(filepath):
+						print(f"[FTpy] ERROR: {filename} does not exist.")
+					filesize = os.path.getsize(filepath)
+					header_sendall(sock,UPLOAD_MESSAGE.encode(FORMAT))
+					header_sendall(sock,filename.encode(FORMAT))
+					header_sendall(sock,str(filesize).encode(FORMAT))
+					left = filesize
+					with open(filepath,"rb") as file:
+						while left > 0:
+							if left < BUFFER_SIZE:
+								sock.sendall(file.read(left))
+								left = 0
+							else:
+								sock.sendall(file.read(BUFFER_SIZE))
+								left-=BUFFER_SIZE
+				elif command == "exit":
+					break
+				else:
+					print("Invalid command.")
+
+
 				
 
 
--- ftpy/server.py
+++ ftpy/server.py
@@ -5,7 +5,8 @@ from utils import *
 
 LISTENING_HOST = ''
 LISTENING_PORT = 9001
-FILES = "ftpy/files"
+FILES = os.getcwd()+"/server-files/"
+print(os.path.exists(FILES))
 
 def handle_client(conn,addr):
     print(f"[FTpy] Established connected from {addr}")
@@ -18,6 +19,38 @@ def handle_client(conn,addr):
             file_list = "\n".join([file for file in os.listdir(FILES)])
             e_file_list = file_list.encode(FORMAT)
             header_sendall(conn,e_file_list)
+        elif cmd == DOWNLOAD_MESSAGE:
+            filename = header_recv(conn).decode(FORMAT).strip()
+            filepath = FILES+filename
+            if not os.path.isfile(filepath):
+                connected = False
+                header_sendall(conn,str(0).encode(FORMAT))
+                continue
+            filesize = os.path.getsize(filepath)
+            print(filesize)
+            header_sendall(conn,str(filesize).encode(FORMAT))
+            left = filesize
+            with open(filepath,"rb") as file:
+                while left > 0:
+                    if left < BUFFER_SIZE:
+                        conn.sendall(file.read(left))
+                        left = 0
+                    else:
+                        conn.sendall(file.read(BUFFER_SIZE))
+                        left-=BUFFER_SIZE
+        elif cmd == UPLOAD_MESSAGE:
+            filename = header_recv(conn).decode(FORMAT).strip()
+            filesize = header_recv(conn).decode(FORMAT).strip()
+            filesize = int(filesize)
+            with open(FILES+filename,"wb") as newfile:      
+                left = filesize
+                while left > 0:
+                    if left < BUFFER_SIZE:
+                        newfile.write(conn.recv(left))
+                        left = 0
+                    else:
+                        newfile.write(conn.recv(BUFFER_SIZE))
+                        left-=BUFFER_SIZE
 
     conn.close()
     print(f"[FTpy] {addr} disconnected")
